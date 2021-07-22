0e2b7ce7bc73f243691c540afd98a4564a6923c0
tariking <s45.rita@gmail.com>
2021-07-22 14:03:46 +0900

    fix tag

--- src/components/tag.js
+++ src/components/tag.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import _ from 'lodash'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTag } from '@fortawesome/free-solid-svg-icons'
 
 const Tag = props => {
+  library.add(faTag)
 
   return (
     <div className={props.index ? 'tag-index' : 'tag'}>
@@ -13,7 +18,9 @@ const Tag = props => {
             key={index}
             className={props.index ? 'tag__listindex' : 'tag__list'}
           >
-            #{tag}
+            <FontAwesomeIcon icon={faTag} style={{
+                  marginRight: 1.3,
+                }}/>{tag}
           </Link>
         )
       })}
